8ff4a791bd6167fe148ea5d685ddff6ef52ab065
Bryan Y <xpreyx@gmail.com>
2020-03-02 21:55:13 -0800

    update connection

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ const { createConnection } = require('mysql2')
 
 // const connection = createConnection(process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : process.env.LOCAL_CONN)
 const connection = createConnection({
-  host: 'localhost',
+  host: 'kil9uzd3tgem3naa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   port: 3306,
-  user: 'root',
-  password: 'rootroot',
-  database: 'burgers_db'
+  user: 'zjwiekeycjgjabh5',
+  password: 'n3edil8tijyx8zpa',
+  database: 'viu1ojst850pbzk4'
 })
 
 module.exports = connection
